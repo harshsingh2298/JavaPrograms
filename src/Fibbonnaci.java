1405c7549599d19d64b2f7a88b4ad07b796857c8
Harsh <harshsingh2298@gmail.com>
2026-02-27 08:30:09 +0530

    27-02-2026

--- src/Fibbonnaci.java
+++ src/Fibbonnaci.java
@@ -1,8 +1,15 @@
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Fibbonnaci {
     public static void main(String[] args) {
+
+
+        int [] arr = new int[]{1,2,3,4,5,6,7,8,9};
+        int start = 0;
+        int end = arr.length-1;
+
         int num = 20;
         int a = 0;
         int b = 1;
@@ -10,6 +17,8 @@ public class Fibbonnaci {
 
         Fibbrecursion fibbrecursion = new Fibbrecursion();
         int print = fibbrecursion.fib(num);
+        int[] reversed = fibbrecursion.reverse(arr,start,end);
+        System.out.println(Arrays.toString(reversed));
         System.out.println("Recursion fib "+print);
 
         for (int i=0; i<=num-1;i++){
@@ -45,16 +54,24 @@ public class Fibbonnaci {
                 .map(f -> f[0])
                 .forEach(n -> System.out.println(n));
 
-
-
-
     }
-
 
 }
 //        ======================================== Using Recursion ======================================================
  class Fibbrecursion {
+    int [] reverse (int [] arr,int start,int end){
+        if (start>=end){
+            return arr;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
 
+        reverse(arr,start+1,end-1);
+
+        return arr;
+
+    }
     int fib(int num) {
         if (num <= 1) {
 
