e165f07e3cf64560ef7dffce8e6472b8d85b715f
GIT_GUI_DEMO <harshsingh2298@gmail.com>
2026-03-10 15:30:11 +0530

    10-03-2026

---
+++ src/DayTwo.java
@@ -0,0 +1,21 @@
+public class DayTwo {
+    public static void main(String[] args) {
+//        Convert Celsius to Fahrenheit.
+//
+//        Convert Fahrenheit to Celsius.
+//
+//        Convert kilometers to meters.
+//
+//        Convert meters to centimeters.
+//
+//        Find the largest of two numbers.
+//
+//        Find the smallest of two numbers.
+//
+//        Check whether a number is even or odd.
+//
+//        Find the sum of digits of a number.
+//
+//        Reverse a number.
+    }
+}
