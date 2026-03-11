ac3b7fc7096be5452cfe85a06e7ca42b19a0b345
GIT_GUI_DEMO <harshsingh2298@gmail.com>
2026-03-11 17:20:26 +0530

    11-03-2026

--- src/DayTwo.java
+++ src/DayTwo.java
@@ -3,10 +3,34 @@ public class DayTwo {
 //        Convert Celsius to Fahrenheit.
 //
 //        Convert Fahrenheit to Celsius.
+
+
+
+        double celsius = 32;
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println("Celsius to Fahrenheit: " + fahrenheit);
+
+        double f = 77;
+        double c = (f - 32) * 5/9;
+
+        System.out.println("Fahrenheit to Celsius: " + c);
 //
 //        Convert kilometers to meters.
 //
 //        Convert meters to centimeters.
+
+
+
+        double kilometers = 5;
+        double meters = kilometers * 1000;
+
+        System.out.println("Kilometers to Meters: " + meters);
+
+        double m = 10;
+        double centimeters = m * 100;
+
+        System.out.println("Meters to Centimeters: " + centimeters);
 //
 //        Find the largest of two numbers.
 //
